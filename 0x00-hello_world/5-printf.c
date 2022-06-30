3f41ba3d243501e8d19273b47e1f221a012b66f3
Kero Filicha <kerotulu@gmai.com>
2022-06-30 13:49:31 -0700

    size

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  * main - Prints out a string to stdout.
  * Return: 0 if success.
  */
- int main(void)
-{ 
-	printf("with proper grammar, but the outcome is a piece of art,\n"); 
-	return (0); 
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n"); 
+return (0); 
 }
