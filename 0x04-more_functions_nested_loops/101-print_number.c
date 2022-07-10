325a00df28b3fc35830432365052247e95e48772
Kero Filicha <kerotulu@gmai.com>
2022-07-10 14:43:59 -0700

    Numbers have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,9 +1,11 @@
+#include <stdio.h>
 #include "main.h"
+
 /**
- * * print_number - Prints a number
- * * @n: The number to print
- * *
- * */
+ * print_number - Prints a number
+ * @n: The number to print
+ */
+
 void print_number(int n)
 {
 	unsigned int num = n;
