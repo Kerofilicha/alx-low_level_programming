43fb3bbd77df720a6767383380c3d6e3bb24f49e
Kero Filicha <kerotulu@gmai.com>
2022-07-10 15:21:42 -0700

    Numbers have life

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,15 +8,15 @@
 
 void print_number(int n)
 {
-	unsigned int num = n;
+	unsigned int num;
+
+	num = n;
 	if (n < 0)
 	{
-		_putchar('-');
+		_putchar(45);
 		num = -num;
 	}
-	if (num > 9)
-	{
+	if (num / 10)
 		print_number(num / 10);
-	}
 	_putchar(num % 10 + '0');
 }
