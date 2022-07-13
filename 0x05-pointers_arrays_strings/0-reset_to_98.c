a97a3e361976b870696be7a4c19e113e55335c80
Kero Filicha <kerotulu@gmai.com>
2022-07-12 18:50:39 -0700

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ *
+ * reset_to_98 - main
+ * @n: - pinter to int
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This is my radm fil for 0x05-pointers_arrays_strings project
