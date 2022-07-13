9f993073e31490b6487d0da7be284120c995adac
Kero Filicha <kerotulu@gmai.com>
2022-07-12 19:23:48 -0700

    defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,16 +1,17 @@
 #include "main.h"
 
 /**
- *
- * swap_int - swaps the values of a and b
- * @a: pointer 1
- * @b: pointer 2
+ * swap_int - main
+ * @a: pointer 
+ * @b: pointer 
  * Return; 0
  */
 
 void swap_int(int *a, int *b)
 {
-	*a += *b;
-	*b = *a - *b;
-	*a = *a - *b;
+	int swap;
+
+	swap = *a;
+	*a = *b;
+	*b = swap;
 }
