d54dde773c04e41d92d01a94b31b6d6bac18ac99
Kero Filicha <kerotulu@gmail.com>
2022-08-16 17:50:13 -0700

    Lists

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,6 +18,18 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
+/**
+ * struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ *
+ * Description: singly linked list of pointers
+ */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
 
 /* Function protoypes */
 size_t free_listint_safe(listint_t **h);
