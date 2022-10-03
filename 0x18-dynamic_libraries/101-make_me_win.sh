efa81dfe7775f97047b7e779286db9f34e02eec0
Kero Filicha <kerotulu@gmail.com>
2022-10-03 15:26:07 -0700

    code injection

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-gcc *.o -fPIC -shared -o libruin.so
+wget -p pwd https://raw.https://https://github.com/kerotulu/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libruin.so
 export LD_PRELOAD=$PWD/libruin.so
