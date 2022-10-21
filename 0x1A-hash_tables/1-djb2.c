60151c609351ebf6cfd9bf5c4302bfaf76cffe20
Kero Filicha <kerotulu@gmail.com>
2022-10-21 16:15:52 -0700

    function implementing the djb2 algorithm

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -6,6 +6,7 @@
  *  
  *  Return: Integer after the operations.
  */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
 	unsigned long int hash;
@@ -13,8 +14,7 @@ unsigned long int hash_djb2(const unsigned char *str)
 
 	hash = 5381;
 	while ((c = *str++))
-	{
-		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-	}
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + count */
+
 	return (hash);
 }
