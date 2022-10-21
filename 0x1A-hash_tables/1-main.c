fa5446d887ea1119f98f52b0f32cccceeaa3d1d0
Kero Filicha <kerotulu@gmail.com>
2022-10-21 16:08:18 -0700

    main

--- 0x1A-hash_tables/1-main.c
+++ 0x1A-hash_tables/1-main.c
@@ -4,15 +4,18 @@
 #include "hash_tables.h"
 
 /**
- *  main - check the code for Holberton School students.
+ * main - check the code
  *
- *  Return: Always EXIT_SUCCESS.
+ * Return: Always EXIT_SUCCESS.
  */
 int main(void)
 {
-	hash_table_t *ht;
-
-	ht = hash_table_create(1024);
-	printf("%p\n", (void *)ht);
+	char *s;
+	s = "cisfun";
+	printf("%lu\n", hash_djb2((unsigned char *)s));
+	s = "Don't forget to tweet today";
+	printf("%lu\n", hash_djb2((unsigned char *)s));
+	s = "98";
+	printf("%lu\n", hash_djb2((unsigned char *)s));
 	return (EXIT_SUCCESS);
 }
